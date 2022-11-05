9b3db21494c036a4c40c4a95ca1f5d0251c5d3c2
Yan Gong <gongyan1920@163.com>
2022-11-05 02:06:36 +0000

    Update train.py

--- VSEpp_LSEH/train.py
+++ VSEpp_LSEH/train.py
@@ -18,16 +18,8 @@ import numpy as np
 import random
 
 
-def set_seed(seed):
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-    torch.backends.cudnn.deterministic = True
-
 
 def main():
-    set_seed(7)
 
     # Hyper Parameters
     parser = argparse.ArgumentParser()
